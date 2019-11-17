503d073755317af0b225cfa5cf12e2646bd1d793
Fabian Golser <fabian.golser@student.tugraz.at>
2019-11-17 22:16:57 +0100

    - fix chaos

--- GGOS_pro1.py
+++ GGOS_pro1.py
@@ -251,30 +251,6 @@ for index in range(z-1):
     # print("c_s = {}".format(c_s))
     matrix[0, 0] = (np.sqrt(1 / 3) * c_s[0]) - c_s[3]
     matrix[0, 1] = -c_s[4]
-
-    """ w x h(t) """
-    w_x_h = np.cross(w, h_v[index, :] - delta_h)
-
-    """ w_dot = F^(-1) * [M - ((D*T_G/Dt) * w) - (w x (T * w)) - (w x h) - (D_h/Dt)] """
-    """ w_dot = F^(-1) * [M - (DT_G_Dt_w)      - (w_x_Tw)      - (w x h) - (D_h/Dt)] """
-    w_dot = np.dot(f_invers, (m - dT_G_Dt_w - w_x_tw - w_x_h ))
-    w_dot_v[index, :] = w_dot
-    #print('w_dot({}) = \n{}'.format(index, w_dot_v[index, :]))
-    
-    w = w + w_dot * 3600
-    w_v[index + 1, :] = w
-    #print('w_v({}) = \n{}'.format(index, w_v[index, :]))
-
-#______________________________________________________________________________
-    # polar_motion
-    """ x_p(t) = (R/W_N) * w_x(t), y_p(t) = (R/W_N) * w_y(t) """
-    w = w_v[index]
-    
-    x_p = (R_earth / Omega_n) * w[0]
-    y_p = (R_earth / Omega_n) * w[1]
-    polar_v[index][0] = x_p
-    polar_v[index][1] = y_p
-
     matrix[0, 2] = -c_s[1]
     matrix[1, 0] = -c_s[4]
     matrix[1, 1] = (np.sqrt(1 / 3) * c_s[0]) + c_s[3]
@@ -376,30 +352,6 @@ for index in range(z-1):
 
     """ w x (T * w) """
     tw = np.dot(t, w)
-
-    """ w x h(t) """
-    w_x_h = np.cross(w, h_v[index, :] - delta_h)
-
-    """ w_dot = F^(-1) * [M - ((D*T_G/Dt) * w) - (w x (T * w)) - (w x h) - (D_h/Dt)] """
-    """ w_dot = F^(-1) * [M - (DT_G_Dt_w)      - (w_x_Tw)      - (w x h) - (D_h/Dt)] """
-    w_dot = np.dot(f_invers, (m - dT_G_Dt_w - w_x_tw - w_x_h ))
-    w_dot_v[index, :] = w_dot
-    #print('w_dot({}) = \n{}'.format(index, w_dot_v[index, :]))
-    
-    w = w + w_dot * 3600
-    w_v[index + 1, :] = w
-    #print('w_v({}) = \n{}'.format(index, w_v[index, :]))
-
-#______________________________________________________________________________
-    # polar_motion
-    """ x_p(t) = (R/W_N) * w_x(t), y_p(t) = (R/W_N) * w_y(t) """
-    w = w_v[index]
-    
-    x_p = (R_earth / Omega_n) * w[0]
-    y_p = (R_earth / Omega_n) * w[1]
-    polar_v[index][0] = x_p
-    polar_v[index][1] = y_p
- 
     w_x_tw = np.cross(w, tw)
 
     """ w x h(t) """
@@ -433,14 +385,13 @@ for index in range(z-1):
  
 #______________________________________________________________________________
     # delta_lod     
-    delta_lod_v[index] = 86400 * ((Omega_n - earth_rot[index][2]) / Omega_n)
+    delta_lod_v[index] = 86400 * ((Omega_n - w_v[index][2]) / Omega_n)
     
     # delta_lod ref    
     delta_lod_v_ref[index] = 86400 * ((Omega_n - earth_rot[index][2]) / Omega_n)
 
 # Don't touch it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # ____________________________________________________________________________
-
 
 
 # w
@@ -485,7 +436,7 @@ ax.set_title('Simulated earth- rotation polar_v_ref', fontsize=19, fontweight="b
 delta_lod_v = delta_lod_v[1:len(delta_lod_v)-2]
 fig = plt.figure(5)
 ax = plt.axes()
-ax.plot(delta_lod_v, range(0, len(delta_lod_v)), 'blue')
+ax.plot(range(0, len(delta_lod_v)), delta_lod_v, 'blue')
 ax.set_xlabel("$\omega_x$ [rad/s * 10^00]",fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$\omega_y$ [rad/s * 10^00]",fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('Simulated earth- rotation delta_lod_v', fontsize=19, fontweight="bold")
@@ -494,14 +445,12 @@ ax.set_title('Simulated earth- rotation delta_lod_v', fontsize=19, fontweight="b
 delta_lod_v_ref = delta_lod_v_ref[1:len(delta_lod_v_ref)-2]
 fig = plt.figure(6)
 ax = plt.axes()
-ax.plot(delta_lod_v_ref, range(0, len(delta_lod_v_ref)), 'red')
+ax.plot(range(0, len(delta_lod_v_ref)), delta_lod_v_ref, 'red')
 ax.set_xlabel("$\omega_x$ [rad/s * 10^00]",fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$\omega_y$ [rad/s * 10^00]",fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('Simulated earth- rotation delta_lod_v_ref', fontsize=19, fontweight="bold")
 
-
-
-
+plt.show()
 
 
 
