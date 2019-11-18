847a19e7630bac7eda05682ddbc18748c8f3f507
Fabian Golser <fabian.golser@student.tugraz.at>
2019-11-18 01:06:49 +0100

    - add special computation: divide G by 2 - save all plots as *.pdf

--- GGOS_pro1.py
+++ GGOS_pro1.py
@@ -11,6 +11,9 @@ import ggosPlot as g_plot
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
+special_computation = True
+#special_computation = False
+
 plt.close('all')
 
 class Data:
@@ -30,7 +33,7 @@ class Data:
         self.read_isdc_files()
         self.interpolate()
         self.w = np.array([self.earth_rotation(0), ])
-        self.w_dot = np.array([[0, 0, 0], ])    # maybe not initialized correct yet
+        self.w_dot = np.array([[0, 0, 0], ])
 
     def append_w(self, w_new):
         """appends the current w(x, y, z)"""
@@ -159,6 +162,8 @@ class Data:
 test = Data()
 z = len(test.moon_)
 G = 6.674*10**(-11)
+if special_computation:
+    G = 6.674 * 10 ** (-11) / 2
 k_Re = 0.3077
 k_Im = 0.0036
 GM_sun = 1.32712442076*10**(20)
@@ -198,6 +203,206 @@ oam[:, 4] = oam[:, 4] + 8.226002041060833*10**(-8) #e-08
 oam[:, 5] = oam[:, 5] + 3.462197020095727*10**(-9) #e-09
 
 
+########################################################################################################################
+if special_computation:
+    w = [0, 0, 0]
+    w_v = np.zeros([z, 3])
+    w_v[0, :] = earth_rot[0, :]
+    w_dot_v = np.zeros([z, 3])
+    tg_v = np.zeros([z, 3, 3])
+    tr_v = np.zeros([z, 3, 3])
+    h_v = np.zeros([z, 3])
+    delta_t = np.zeros([3, 3])
+    delta_h = np.zeros([3, ])
+    polar_v = np.zeros([z, 2])
+    delta_lod_v = np.zeros([z, ])
+    polar_v_ref = np.zeros([z, 2])
+    delta_lod_v_ref = np.zeros([z, ])
+
+    # ______________________________________________________________________________
+    # main loop
+    for index in range(z - 1):
+        # ___ sun
+        M_sun = np.zeros([3, ])
+        body = sun[index]
+        GM = GM_sun
+        r = np.sqrt(body[0] ** 2 + body[1] ** 2 + body[2] ** 2)
+        scalar = 3 * GM / r ** 5
+        M_sun[0] = body[1] * body[2] * (C_strich - A_B_strich)
+        M_sun[1] = body[0] * body[2] * (A_B_strich - C_strich)
+        M_sun[2] = body[0] * body[1] * (A_B_strich - A_B_strich)
+        M_sun = scalar * M_sun
+
+        # ___ moon
+        M_moon = np.zeros([3, ])
+        body = moon[index]
+        GM = GM_moon
+        r = np.sqrt(body[0] ** 2 + body[1] ** 2 + body[2] ** 2)
+        scalar = 3 * GM / r ** 5
+        M_moon[0] = body[1] * body[2] * (C_strich - A_B_strich)
+        M_moon[1] = body[0] * body[2] * (A_B_strich - C_strich)
+        M_moon[2] = body[0] * body[1] * (A_B_strich - A_B_strich)
+        M_moon = scalar * M_moon
+
+        # ___ sum
+        m = np.zeros([3, ])
+        m = M_moon + M_sun
+        # ______________________________________________________________________________
+        """ T_G(t) = sqrt(5/3)*M*R^2*(matrix) """
+        scalar = np.sqrt(5 / 3) * M_earth * R_earth ** (2)
+        # print("scalar = {}".format(scalar))
+
+        matrix = np.zeros([3, 3])
+        c_s = pc_aohis[index]
+        c_s = c_s + pc_tide[index]
+        # print("c_s = {}".format(c_s))
+        matrix[0, 0] = (np.sqrt(1 / 3) * c_s[0]) - c_s[3]
+        matrix[0, 1] = -c_s[4]
+        matrix[0, 2] = -c_s[1]
+        matrix[1, 0] = -c_s[4]
+        matrix[1, 1] = (np.sqrt(1 / 3) * c_s[0]) + c_s[3]
+        matrix[1, 2] = -c_s[2]
+        matrix[2, 0] = -c_s[1]
+        matrix[2, 1] = -c_s[2]
+        matrix[2, 2] = -(2 * np.sqrt(1 / 3) * c_s[0])
+        # print("matrix = \n{}".format(matrix))
+
+        # tr_
+        tr_ = A_B_strich + A_B_strich + C_strich
+        # print("tr_ = {}".format(tr_))
+        matrix_2 = np.eye(3)
+        matrix_2 = matrix_2 * (tr_ / 3)
+        # print("matrix_2 = \n{}".format(matrix_2))
+
+        tg = (scalar * matrix) + matrix_2
+        tg_v[index, :] = tg
+
+        # ______________________________________________________________________________
+        """ T_R(t) = (O_N*R^5)/(3*G) * (matrix) """
+        tr_scalar = (Omega_n * R_earth ** (5)) / (3 * G)
+        # print("scalar = {}".format(scalar))
+
+        # matrix
+        tr_matrix = np.zeros([3, 3])
+        w_x = w_v[index][0]
+        w_y = w_v[index][1]
+
+        tr_matrix[0, 2] = (k_Re * w_x + k_Im * w_y)
+        tr_matrix[1, 2] = (k_Re * w_y - k_Im * w_x)
+        tr_matrix[2, 0] = (k_Re * w_x + k_Im * w_y)
+        tr_matrix[2, 1] = (k_Re * w_y - k_Im * w_x)
+        # print("matrix = \n{}".format(matrix))
+
+        tr = tr_scalar * tr_matrix
+        tr_v[index, :] = tr
+
+        # ______________________________________________________________________________
+        """ T(t) = T_G(t) + T_R(t) """
+        t = tg_v[index, :] + tr_v[index, :]
+
+        # ______________________________________________________________________________
+        # h
+        h_x = aam[index, :] + oam[index, :] + ham[index, :]
+
+        h = np.zeros([3, ])
+        # h[0] = ((Omega_n * (C_strich - A_B_strich)) / 1.610) * h_x[3]
+        # h[1] = ((Omega_n * (C_strich - A_B_strich)) / 1.610) * h_x[4]
+        # h[2] = ((Omega_n *  C_strich) / 1.125) * h_x[5]
+
+        h[0] = (1.610 / (Omega_n * (C_strich - A_B_strich))) * h_x[3]
+        h[1] = (1.610 / (Omega_n * (C_strich - A_B_strich))) * h_x[4]
+        h[2] = (1.125 / (Omega_n * C_strich)) * h_x[5]
+
+        h_v[index, :] = h / 3600
+
+        # ______________________________________________________________________________
+        """ dTG(t) = T_G(t) - T_R(t-1) """
+        if index > 0:
+            delta_t = tg_v[index] - tg_v[index - 1]
+        # else:
+        #    delta_t = tg_v[index] - 0
+        delta_t = delta_t / 3600
+
+        # ______________________________________________________________________________
+        """ dTG(t) = T_G(t) - T_R(t-1) """
+        if index > 0:
+            delta_h = h_v[index] - h_v[index - 1]
+        else:
+            delta_h = h_v[index] / 2
+        delta_h = delta_h / 3600
+
+        # ______________________________________________________________________________
+        """ force """
+        f_scalar = (Omega_n ** 2 * R_earth ** (5)) / (3 * G)
+
+        # matrix
+        f_matrix = np.zeros([3, 3])
+        f_matrix[0, 0] = k_Re
+        f_matrix[0, 1] = k_Im
+        f_matrix[1, 0] = -k_Im
+        f_matrix[1, 1] = k_Re
+        # print("matrix = \n{}".format(matrix))
+
+        t_temp = f_scalar * f_matrix
+        f = t + t_temp
+        # ______________________________________________________________________________
+        # w_dot
+
+        w = w_v[index, :]
+        """ F^(-1) """
+        f_invers = np.linalg.inv(f)
+
+        """ (D*T_G/Dt) * w """
+        dT_G_Dt_w = np.dot(delta_t, w)
+        # print('delta_t({}) = \n{}'.format(index, delta_t))
+
+        """ w x (T * w) """
+        tw = np.dot(t, w)
+        w_x_tw = np.cross(w, tw)
+
+        """ w x h(t) """
+        w_x_h = np.cross(w, h_v[index, :] - delta_h)
+
+        """ w_dot = F^(-1) * [M - ((D*T_G/Dt) * w) - (w x (T * w)) - (w x h) - (D_h/Dt)] """
+        """ w_dot = F^(-1) * [M - (DT_G_Dt_w)      - (w_x_Tw)      - (w x h) - (D_h/Dt)] """
+        w_dot = np.dot(f_invers, (m - dT_G_Dt_w - w_x_tw - w_x_h))
+        w_dot_v[index, :] = w_dot
+        # print('w_dot({}) = \n{}'.format(index, w_dot_v[index, :]))
+
+        w = w + w_dot * 3600
+        w_v[index + 1, :] = w
+        # print('w_v({}) = \n{}'.format(index, w_v[index, :]))
+
+        # ______________________________________________________________________________
+        # polar_motion
+        """ x_p(t) = (R/W_N) * w_x(t), y_p(t) = (R/W_N) * w_y(t) """
+        w = w_v[index]
+
+        x_p = (R_earth / Omega_n) * w[0]
+        y_p = (R_earth / Omega_n) * w[1]
+        polar_v[index][0] = x_p
+        polar_v[index][1] = y_p
+
+        # polar_motion ref
+        w = earth_rot[index]
+        polar_v_ref[index][0] = (R_earth / Omega_n) * w[0]
+        polar_v_ref[index][1] = (R_earth / Omega_n) * w[1]
+
+        # ______________________________________________________________________________
+        # delta_lod
+        delta_lod_v[index] = 86400 * ((Omega_n - w_v[index][2]) / Omega_n)
+
+        # delta_lod ref
+        delta_lod_v_ref[index] = 86400 * ((Omega_n - earth_rot[index][2]) / Omega_n)
+
+    # Don't touch it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ########################################################################################################################
+    # save special computation matrices, restore original values and rerun simulation
+    w_v_g_half = w_v
+    G = 6.674 * 10 ** (-11)
+
+
+########################################################################################################################
 w = [0, 0, 0]
 w_v = np.zeros([z, 3])
 w_v[0, :] = earth_rot[0, :]
@@ -391,7 +596,7 @@ for index in range(z-1):
     delta_lod_v_ref[index] = 86400 * ((Omega_n - earth_rot[index][2]) / Omega_n)
 
 # Don't touch it!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# ____________________________________________________________________________
+########################################################################################################################
 
 
 # w
@@ -407,6 +612,7 @@ ax.set_xlabel("$\omega_x$ [rad/s * $10^{-10}$]" ,fontsize=15, fontweight="bold",
 ax.set_ylabel("$\omega_y$ [rad/s * $10^{-10}$]" ,fontsize=15, fontweight="bold", labelpad=10)
 ax.set_zlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('Simulated earth rotation', fontsize=19, fontweight="bold")
+fig.savefig("earth_rotation.pdf", bbox_inches='tight')
 
 # w ref
 earth_rot = earth_rot[1:len(earth_rot)-2]
@@ -417,58 +623,81 @@ ax.set_xlabel("$\omega_x$ [rad/s * $10^{-10}$]", fontsize=15, fontweight="bold",
 ax.set_ylabel("$\omega_y$ [rad/s * $10^{-10}$]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_zlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('ref earth rotation', fontsize=19, fontweight="bold")
+fig.savefig("earth_rotation_ref.pdf", bbox_inches='tight')
 
 # w & w ref
 fig = plt.figure(3)
 ax = plt.axes(projection='3d', xlim=polar_lim_x, ylim=polar_lim_y, zlim=polar_lim_z)
-ax.plot3D(w_v[:, 0], w_v[:, 1], np.linspace(start=2005, stop=2015, num=len(w_v)), 'red', lw=line_width_polar)
-ax.plot3D(earth_rot[:, 0], earth_rot[:, 1], np.linspace(start=2005, stop=2015, num=len(w_v)), 'blue', lw=line_width_polar)
+ax.plot3D(w_v[:, 0], w_v[:, 1], np.linspace(start=2005, stop=2015, num=len(w_v)), 'red', lw=line_width_polar, label='sim')
+ax.plot3D(earth_rot[:, 0], earth_rot[:, 1], np.linspace(start=2005, stop=2015, num=len(w_v)), 'blue', lw=line_width_polar, label='ref')
 ax.set_xlabel("$\omega_x$ [rad/s * $10^{-10}$]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$\omega_y$ [rad/s * $10^{-10}$]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_zlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('Comparison Simulated- and ref earth rotation', fontsize=19, fontweight="bold")
+plt.legend()
+fig.savefig("Comparison_simulated_earth_rotation_with_ref.pdf", bbox_inches='tight')
+
+if special_computation:
+    # w & w g_half
+    w_v_g_half = w_v_g_half[1:len(w_v_g_half)-2]
+    fig = plt.figure(4)
+    ax = plt.axes(projection='3d', zlim=polar_lim_z)
+    ax.plot3D(w_v[:, 0], w_v[:, 1], np.linspace(start=2005, stop=2015, num=len(w_v)), 'red', lw=line_width_polar, label='G')
+    ax.plot3D(w_v_g_half[:, 0], w_v_g_half[:, 1], np.linspace(start=2005, stop=2015, num=len(w_v)), 'green', lw=line_width_polar, label='G/2')
+    ax.set_xlabel("$\omega_x$ [rad/s * $10^{-10}$]", fontsize=15, fontweight="bold", labelpad=10)
+    ax.set_ylabel("$\omega_y$ [rad/s * $10^{-10}$]", fontsize=15, fontweight="bold", labelpad=10)
+    ax.set_zlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
+    ax.set_title('Comparison simulated earth rotation with normal and half G', fontsize=19, fontweight="bold")
+    plt.legend()
+    fig.savefig("Comparison_earth_rotation_with_normal_and_half_g.pdf", bbox_inches='tight')
 
 
 # polar
 polar_m_lim_x = (-6, 10)
 polar_m_lim_y = (-18, -2)
 polar_v = polar_v[1:len(polar_v)-2]
-fig = plt.figure(4)
+fig = plt.figure(5)
 ax = plt.axes(xlim=polar_m_lim_x, ylim=polar_m_lim_y)
 ax.plot(polar_v[:, 0], polar_v[:, 1], 'red', lw=line_width_polar)
 ax.set_xlabel("$x_p$ [m]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$y_p$ [m]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('Simulated polar motion', fontsize=19, fontweight="bold")
+fig.savefig("polar_motion.pdf", bbox_inches='tight')
 
 # polar ref
 polar_v_ref = polar_v_ref[1:len(polar_v_ref)-2]
-fig = plt.figure(5)
+fig = plt.figure(6)
 ax = plt.axes(xlim=polar_m_lim_x, ylim=polar_m_lim_y)
 ax.plot(polar_v_ref[:, 0], polar_v_ref[:, 1], 'blue', lw=line_width_polar)
 ax.set_xlabel("$x_p$ [m]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$y_p$ [m]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('ref polar motion', fontsize=19, fontweight="bold")
+fig.savefig("polar_motion_ref.pdf", bbox_inches='tight')
 
 
 # polar x & polar x ref
 polar_m_lim_x_comp = (-10, 15)
 polar_m_lim_y_comp = (-20, 0)
-fig = plt.figure(6)
+fig = plt.figure(7)
 ax = plt.axes(xlim=polar_lim_z, ylim=polar_m_lim_x_comp)
-ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v[:, 0], 'red', lw=line_width_polar)
-ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v_ref[:, 0], 'blue', lw=line_width_polar)
+ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v[:, 0], 'red', lw=line_width_polar, label='sim')
+ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v_ref[:, 0], 'blue', lw=line_width_polar, label='ref')
 ax.set_xlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$x_p$ [m]", fontsize=15, fontweight="bold", labelpad=10)
-ax.set_title('polar motion comparison', fontsize=19, fontweight="bold")
+ax.set_title('polar motion comparison x', fontsize=19, fontweight="bold")
+plt.legend()
+fig.savefig("polar_motion_comparison_x.pdf", bbox_inches='tight')
 
 # polar y & polar y ref
-fig = plt.figure(7)
+fig = plt.figure(8)
 ax = plt.axes(xlim=polar_lim_z, ylim=polar_m_lim_y_comp)
-ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v[:, 1], 'red', lw=line_width_polar)
-ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v_ref[:, 1], 'blue', lw=line_width_polar)
+ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v[:, 1], 'red', lw=line_width_polar, label='sim')
+ax.plot(np.linspace(start=2005, stop=2015, num=len(polar_v)), polar_v_ref[:, 1], 'blue', lw=line_width_polar, label='ref')
 ax.set_xlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$y_p$ [m]", fontsize=15, fontweight="bold", labelpad=10)
-ax.set_title('polar motion comparison', fontsize=19, fontweight="bold")
+ax.set_title('polar motion comparison y', fontsize=19, fontweight="bold")
+plt.legend()
+fig.savefig("polar_motion_comparison_y.pdf", bbox_inches='tight')
 
 
 # delta_lod
@@ -481,6 +710,7 @@ ax.plot(np.linspace(start=2005, stop=2015, num=len(delta_lod_v)), delta_lod_v, '
 ax.set_xlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$\Delta$ LOD [s]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('Simulated $\Delta$ LOD', fontsize=19, fontweight="bold")
+fig.savefig("delta_lod_sim.pdf", bbox_inches='tight')
 
 # delta_lod ref
 delta_lod_v_ref = delta_lod_v_ref[1:len(delta_lod_v_ref)-2]
@@ -490,6 +720,7 @@ ax.plot(np.linspace(start=2005, stop=2015, num=len(delta_lod_v_ref)), delta_lod_
 ax.set_xlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$\Delta$ LOD [s]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('$\Delta$ LOD', fontsize=19, fontweight="bold")
+fig.savefig("delta_lod_ref.pdf", bbox_inches='tight')
 
 # delta lod ref - delta lod
 delta_delta_lod = delta_lod_v_ref - delta_lod_v
@@ -499,5 +730,6 @@ ax.plot(np.linspace(start=2005, stop=2015, num=len(delta_delta_lod)), delta_delt
 ax.set_xlabel("time [years]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_ylabel("$\Delta$ LOD [s]", fontsize=15, fontweight="bold", labelpad=10)
 ax.set_title('ref $\Delta$ $LOD_{Ref}$ - $\Delta$ $LOD_{Model}$', fontsize=19, fontweight="bold")
+fig.savefig("delta_lod_ref_minus_sim.pdf", bbox_inches='tight')
 
 plt.show()
